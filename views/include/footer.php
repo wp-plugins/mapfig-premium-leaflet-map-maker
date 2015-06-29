9f69125ce8fbd14a90b7b9bc68e0e5221ac475ce
MapFig <MapFig@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-29 14:32:26 +0000

    Version 0.1
    git-svn-id: https://plugins.svn.wordpress.org/mapfig-premium-leaflet-map-maker/trunk@1189642 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/include/footer.php
@@ -0,0 +1,212 @@
+<html>
+ 
+<head>
+<style>
+/*rs style*/
+#rs_main_footer .rs_new_row_bottom {
+     background: none repeat scroll 0 0 #444444;
+    color: #b3b3b3;
+    font-size: 14px;
+  padding-bottom: 10px;
+    padding-top: 10px;
+}
+#rs_main_footer .container{width:1070px !important;}
+#rs_main_footer .rs_footer_s {
+    margin-bottom: 0;
+    text-align: right;
+}
+#rs_main_footer .rs_new_row_top{
+    background: none repeat scroll 0 0 #6dbd63;
+   padding-bottom:20px;
+ margin-top: 20px;
+}
+#rs_main_footer .new_footer_about_p{color:#fff;font-size: 14px;}
+#rs_main_footer .rs_more{color: #377130;
+    font-size: 16px;}
+
+#rs_main_footer .title{   color: #2e5f28;
+    font-size: 22px;
+    font-weight: normal;}
+#rs_main_footer .rs_new_ul li a {
+    color: #377130;
+    font-size: 16px;
+}
+#rs_main_footer .rs_only_right_side p {
+   color: #377130;
+    font-size: 18px;
+}
+#rs_main_footer .rs_eemail a{ color: #377130 !important;
+    font-size: 16px;}
+#rs_main_footer .rs_footer_s li a {
+    color: #b3b3b3;
+    font-size: 20px;
+    margin-left: 7px;
+}
+#rs_main_footer .rs_footer_s li {
+    margin-bottom: 0;
+}
+#rs_main_footer .rs_new_row_bottom .copyright {
+    padding-top: 5px;
+}
+#rs_main_footer .rs_new_ul li {
+    margin-bottom: 10px;
+}
+</style>
+</head> 
+
+
+
+<div style="clear:both;">
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</div>
+<div class="rs_footer3" id="rs_main_footer">
+
+
+<footer2 class="footer2">
+
+        <div class="footer2-content">
+
+            <div class="container">
+                <div class="row rs_new_row_top">
+
+                    <div class="footer2-col col-md-5 col-sm-7 col-sm-12 about">
+
+                        <div class="footer2-col-inner">
+
+                            <h3 class="title">About Us</h3>
+                            <p class="new_footer_about_p">MapFig is produced by AcuGIS. AcuGIS is a world leader in GIS hosting, serving individuals, businesses, research and academic institutions in over 40 countries world wide.  The MapFig brand (MapFig Ltd) is an extension of the AcuGIS philosophy of making GIS simple. We hope you find this plugin both useful and easy to use.  We also welcome your feedback on how we can improve on subsequent versions. </p>
+                            <p><a class="more rs_more" href="https://www.mapfig.com" target="_blank">Learn more about MapFig<i class="fa fa-long-arrow-right"></i></a></p>
+							
+                            
+                        </div><!--//footer-col-inner-->
+                    </div><!--//foooter2-col-->
+                    <div class="footer2-col col-md-3 col-sm-4 col-md-offset-1 links">
+
+                        <div class="footer2-col-inner">
+                            <h3 class="title">Links</h3>
+                            <ul class="list-unstyled rs_new_ul">
+                               <li><a href="http://docs.mapfig.com/wordpress-leaflet-plugin/" target="_blank"><i class="fa fa-caret-right"></i> Plugin Docs</a></li>
+                                <li><a href="https://www.mapfig.com" target="_blank"><i class="fa fa-caret-right"></i> MapFig</a></li>
+				    <li><a href="https://www.mapfig.com/pricing.html" target="_blank"><i class="fa fa-caret-right"></i> MapFig Studio</a></li>
+                                
+                                <li><a href="https://www.acugis.com" target="_blank"><i class="fa fa-caret-right"></i> AcuGIS</a></li>
+
+								
+                            </ul>
+                        </div><!--//footer2-col-inner-->
+                    </div><!--//foooter2-col-->                 
+                    <div class="footer2-col col-md-3 col-sm-12 contact">
+
+                        <div class="footer2-col-inner">
+                            <h3 class="title">Get in touch</h3>
+                            <div class="row rs_only_right_side">
+                                <p class="tel col-md-12 col-sm-4"><i class="fa fa-phone"></i> (USA) 702 922 7130</p>
+								<p class="tel col-md-12 col-sm-4"><i class="fa fa-phone"></i> (UK) 44 208 819 9664</p>
+                                <p class="email rs_eemail col-md-12 col-sm-4"><i class="fa fa-envelope"></i> <a href="#">info@mapfig.com</a></p>
+                                <p class="email rs_eemail col-md-12 col-sm-4"><i class="fa fa-envelope"></i> <a href="#">sales@mapfig.com</a></p>
+                            </div> 
+                        </div><!--//footer2-col-inner-->            
+                    </div><!--//foooter2-col-->   
+                </div><!--//row-->
+            </div><!--//container-->        
+        </div><!--//footer-content-->
+        <div class="bottom-bar2">
+            <div class="container">
+                <div class="row rs_new_row_bottom">
+                    <small class="copyright col-md-6 col-sm-6 col-xs-12">Copyright @ 2015 Enciva LLC, MapFig Ltd.<br> <a href="https://www.mapfig.com" target="_blank">MapFig</a> Studio | <a href="http://www.acugis.com/" target="_blank">AcuGIS</a> GIS Hosting | <a href="https://www.enciva.com" target="_blank">Enciva</a> Oracle |  
+                    <a href="https://www.brainfurnace.com" target="_blank">Brainfurnace</a> PostgreSQL</small>
+                    <ul class="social2 rs_footer_s col-md-6 col-sm-6 col-xs-12 list-inline">
+                        <li><a href="https://twitter.com/mapfig" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="https://www.facebook.com/mapfig" target="_blank"><i class="fa fa-facebook"></i></a></li>                        
+                        <li><a href="https://www.linkedin.com/company/mapfig" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+                        <li class="last"><a href="https://github.com/MapFig" target="_blank"><i class="fa fa-github"></i></a></li>
+                    </ul><!--//social-->
+              </div><!--//row-->
+            </div><!--//container-->
+        </div><!--//bottom-bar-->
+    </footer2>
+
+
+
+</div>
+
+</html>
+
+
+
+<script>
+	function Alert(message, type) {
+		title = ""
+		if(!message)
+			message = "";
+		switch(type) {
+			case "default":
+				type = BootstrapDialog.TYPE_DEFAULT;
+				title = "Information!";
+				break;
+			case "info":
+				type = BootstrapDialog.TYPE_INFO;
+				title = "Information!";
+				break;
+			case "primary":
+				type = BootstrapDialog.TYPE_PRIMARY;
+				title = "Information!";
+				break;
+			case "notify":
+				type = BootstrapDialog.TYPE_PRIMARY;
+				title = "Notification!";
+				break;
+			case "success":
+				type = BootstrapDialog.TYPE_SUCCESS;
+				title = "Success!";
+				break;
+			case "warning":
+				type = BootstrapDialog.TYPE_WARNING;
+				title = "Warning!";
+				break;
+			case "danger":
+			case "error":
+				type = BootstrapDialog.TYPE_DANGER;
+				title = "Error!";
+				break;
+			default :
+				type = BootstrapDialog.TYPE_DEFAULT;
+				title = "Information!";
+		}
+		bd = BootstrapDialog.alert(message);
+		bd.setType(type);
+		bd.setTitle(title);
+	}
+	$(document).ready(function(){
+		$('a.btn.btn-danger').click(function(e){
+			var text = $.trim($(this).text());
+			
+			if(text == "Remove" || text == "Delete" || $(this).find('.fa.fa-remove').length == 1) {
+				var href = $(this).attr('href');
+				e.preventDefault();
+				
+				BootstrapDialog.confirm("Are you Sure you want to delete it?", function(result){
+					if(result) {
+						window.location = href;
+					}
+				});
+			}
+		});
+	});
+</script>
+<style>
+	.breadcrumb-arrow li.pull-right.help a {
+		border: none!important;
+		background: none!important;
+	}
+	.breadcrumb-arrow li.pull-right.help a:before, .breadcrumb-arrow li.pull-right.help a:after {
+		border: none!important;
+	}
+	.breadcrumb-arrow li.pull-right.help a:focus {
+		outline:none;
+	}
+</style>
